40177a073ea42569acf37ec4b4fdada357cf603e
Ooloobooloo <trichuot2000@gmail.com>
2026-04-21 00:22:22 +0700

    fix: update treeRoutes.js

--- src/routes/treeRoutes.js
+++ src/routes/treeRoutes.js
@@ -2,21 +2,14 @@ const express = require('express');
 const router = express.Router();
 const treeController = require('../controllers/treeControllers');
 
-// Home page - list all trees
 router.get('/', treeController.getAllTrees);
 
-// Create tree
 router.post('/trees', treeController.addTree);
 
-// Update tree (chỉ giữ POST vì modal submit bằng form POST)
+// Chỉ giữ POST cho update
 router.post('/trees/edit/:id', treeController.updateTree);
 
-// Delete tree
 router.post('/trees/delete/:id', treeController.deleteTree);
-
-// Reset all trees
 router.post('/reset', treeController.resetAll);
-
-
 
 module.exports = router;
